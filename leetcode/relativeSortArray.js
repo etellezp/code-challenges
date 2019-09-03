b87ac584e770af6594b1318f6c1305660eac985f
github username <etellezp@gmail.com>
2019-09-03 12:45:18 -0400

    Added leetcode folder and leetcode sortRelative challenge

---
+++ leetcode/relativeSortArray.js
@@ -0,0 +1,28 @@
+// Instructions
+//
+// Given two arrays arr1 and arr2, the elements of arr2 are distinct,
+// and all elements in arr2 are also in arr1.
+// 
+// Sort the elements of arr1 such that the relative ordering of items in arr1
+// are the same as in arr2. Elements that don't appear in arr2 should be placed
+// at the end of arr1 in ascending order.
+
+const relativeSortArray = (arr1, arr2) => {
+  let compareArray = []
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.includes(arr2[i])) {
+      compareArray.push(arr1.filter(num => num === arr2[i]))
+    }
+  }
+
+  let flatArray = compareArray.flat()
+
+  let uniqueArray = arr1.filter(num => !flatArray.includes(num))
+
+  console.log(flatArray.concat(uniqueArray))
+}
+
+relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6])
+
+// output => [2,2,2,1,4,3,3,9,6,7,19]
